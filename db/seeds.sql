ad04a4ec1e12462b2f8eeeb1b35ec7ebd6d7a117
Franky Erra <frankyerra43@gmail.com>
2022-07-11 20:18:35 -0700

    created employees table and created seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (name)
+VALUES ("Lion King"),
+       ("The Godfather"),
+       ("West Side Story"),
+       ("Parasite"),
+       ("The Wizard of Oz");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES (1, "Zazu is underrated. Give that hornbill a sequel!"),
+       (2, "I'm gonna make him an offer you can't refuse, watch this movie"),
+       (1, "Scar is the lion everyone loves to hate"),
+       (3, "Ten years of ballet and three years of tap to join a gang in this neighborhood"),
+       (5, "The tin man gave a metallic, hollow performance"),
+       (1, "Hakuna matata"),
+       (5, "Those flying monkeys are nightmare fuel!");
+
+INSERT INTO employees (last_name, first_name, role_id, manager_id)
+VALUES (1, "Zazu is underrated. Give that hornbill a sequel!"),
+       (2, "I'm gonna make him an offer you can't refuse, watch this movie"),
+       (1, "Scar is the lion everyone loves to hate"),
+       (3, "Ten years of ballet and three years of tap to join a gang in this neighborhood"),
+       (5, "The tin man gave a metallic, hollow performance"),
+       (1, "Hakuna matata"),
+       (5, "Those flying monkeys are nightmare fuel!");
+       
