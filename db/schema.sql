0ed5da13aa7461fd24f2661446de68cf4e0374f0
Franky Erra <frankyerra43@gmail.com>
2022-07-13 01:14:00 -0700

    fixed syntax for employees table

--- db/schema.sql
+++ db/schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE roles (
   FOREIGN KEY (department_id) REFERENCES department(id)
 )
 
-CREATE TABLE employees {
+CREATE TABLE employees (
   id INT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
@@ -24,7 +24,7 @@ CREATE TABLE employees {
   manager_id INT,
   FOREIGN KEY (role_id) REFERENCES role(id),
   FOREIGN KEY (manager_id) REFERENCES managers(id)
-}
+)
 
 
 
