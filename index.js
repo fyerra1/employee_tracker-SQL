903208eea89e411acb7fda3ec1aed91194d61fe6
Franky Erra <frankyerra43@gmail.com>
2022-07-13 21:51:05 -0700

    started functions to manage company employees

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 const inquirer = require('inquirer');
 const connection = require('./config/connection');
 const cTable = require('console.table');
+
+const viewDepartments = () => {
+
+};
+
+const viewRoles = () => {
+
+};
+
+const viewEmployees = () => {
+
+};
+
+const addDepartment = () => {
+
+};
+
+const addRole = () => {
+
+};
+
+const addEmployee = () => {
+
+};
+
+const updateRole = () => {
+
+};
+
+
+
